1a69b1b724d1fe9f2014ea57c1ae43f00d752a6d
Felix-Binder <35593162+Felix-Binder@users.noreply.github.com>
2025-04-07 12:58:40 +0200

    ci: set npmPublish: false

--- release.config.js
+++ release.config.js
@@ -5,7 +5,12 @@ export default {
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         'semantic-release-license',
-        '@semantic-release/npm',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: false,
+            },
+        ],
         [
             '@semantic-release/exec',
             {
